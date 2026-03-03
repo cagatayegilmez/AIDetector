7c96bfe6078836c3bd0d282d4e8630a0cf5f5085
cagatayegilmez <cagatayegilmez08@gmail.com>
2026-03-03 13:48:28 +0300

    [AIDetector] Create project base

---
+++ AIDetector/AIDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIDetectorApp.swift
+//  AIDetector
+//
+//  Created by Çağatay Eğilmez on 3.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct AIDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
